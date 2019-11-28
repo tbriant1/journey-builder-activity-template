cc339ac08c4f864d530461dd01b69844d6ff47c2
tbriant1 <58037124+tbriant1@users.noreply.github.com>
2019-11-28 19:22:29 +0000

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -71,7 +71,7 @@ define([
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+            "emailAddress": "{{Contact.Key}}"
         }];
         
         payload['metaData'].isConfigured = true;
